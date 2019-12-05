f7b24d7f0437a4238188ab9781844ea8b4d8d845
ThijsVlaeyen <thijs.vlaeyen@student.ucll.be>
2019-12-05 13:37:16 +0100

    Factory is better en classes for discount have methods now

--- src/model/DiscountThreshold.java
+++ src/model/DiscountThreshold.java
@@ -1,11 +1,23 @@
 package model;
 
-import database.ProductDB;
+import model.IO.LoadSaveProperties;
 
 public class DiscountThreshold implements DiscountStrategy{
 
+   private LoadSaveProperties properties = new LoadSaveProperties();
+
    @Override
-   public int calculateDiscount(ShoppingCart shoppingCart) {
-      return 0;
+   public double calculateDiscount(ShoppingCart shoppingCart) {
+      double discount = 0;
+      String percent = LoadSaveProperties.getDiscountThresholdPercent();
+      percent = percent.substring(0, percent.length()-1);
+      for (Product p: shoppingCart.getItemsList()) {
+         discount += p.getPrice();
+      }
+      if (discount > Integer.parseInt(LoadSaveProperties.getDiscountThresholdAmount())){
+         return discount /100 * Integer.parseInt(percent);
+      } else {
+         return 0;
+      }
    }
 }
