3b919735b954ae4963576043efd87b890f2bb3b5
ThijsVlaeyen <thijs.vlaeyen@student.ucll.be>
2019-12-12 11:15:01 +0100

    there was a small problem with overview controller quick fix not much

--- src/controllers/ProductOverviewController.java
+++ src/controllers/ProductOverviewController.java
@@ -14,32 +14,31 @@ import java.util.Collections;
 import java.util.List;
 
 public class ProductOverviewController implements Observer {
-    private ProductDB db;
-    private ProductOverviewPane view;
-    private LoadSaveProperties properties;
+private ProductDB db;
+private ProductOverviewPane view;
+private LoadSaveProperties properties;
 
-    public ProductOverviewController(ProductDB db){
-        this.db = db;
-        db.addObserver(EventType.PRODUCTSCHANGED, this);
-        properties = new LoadSaveProperties();
-    }
+public ProductOverviewController(ProductDB db) {
+   this.db = db;
+   db.addObserver(EventType.PRODUCTSCHANGED, this);
+   properties = new LoadSaveProperties();
+}
 
-    public ArrayList<Product> getProducts(){
-        LoadSaveStrategyFactory factory = new LoadSaveStrategyFactory();
-        LoadSaveStrategy loadSaveStrategy = factory.createObject(LoadSaveProperties.getLoadSave());
-        db.setLoadSaveStrategy(loadSaveStrategy);
-        ArrayList<Product> products = db.getProducts();
-        Collections.sort(products);
-       return products;
-    }
+public ArrayList<Product> getProducts() {
+   LoadSaveStrategyFactory factory = new LoadSaveStrategyFactory();
+   LoadSaveStrategy loadSaveStrategy = factory.createObject(LoadSaveProperties.getLoadSave());
+   db.setLoadSaveStrategy(loadSaveStrategy);
+   ArrayList<Product> products = db.getProducts();
+   Collections.sort(products);
+   return products;
+}
 
-    public void setView(ProductOverviewPane view){
-        this.view = view;
-    }
+public void setView(ProductOverviewPane view) {
+   this.view = view;
+}
 
-    @Override
-    public void update(Object object) {
-        ShoppingCart cart = (ShoppingCart) object;
-        view.update(cart.getItemsList());
-    }
+@Override
+public void update(Object object) {
+   view.update(getProducts());
+}
 }
